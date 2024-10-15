fddcdf0eb6c576a62f86f687d2623fbc6e5102f1
ardi Titikkata <ardi@ardis-MacBook-Pro.local>
2024-10-15 17:16:09 +0700

    update Cors

--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URI = '/api/users/';
+// const API_URI = '/api/users/';
+const API_URI = 'https://taskapp-8ixvw9ya.b4a.run/api/users/';
 
 const register = async (userData) => {
   const response = await axios.post(API_URI, userData);
